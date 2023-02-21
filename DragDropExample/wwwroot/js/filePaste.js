b723ab6c03c2e270c1e5aa7f21e8eb9ab738b9ed
Felipe Gavilan <elmejor_887@hotmail.com>
2023-02-21 17:57:27 -0400

    Add project files.

---
+++ DragDropExample/wwwroot/js/filePaste.js
@@ -0,0 +1,17 @@
+﻿export function initializeFilePaste(fileDropContainer, inputFile) {
+
+    fileDropContainer.addEventListener('paste', onPaste);
+
+    function onPaste(event) {
+        inputFile.files = event.clipboardData.files;
+        const changeEvent = new Event('change', { bubbles: true });
+        inputFile.dispatchEvent(changeEvent);
+    }
+
+    return {
+        dispose: () => {
+            fileDropContainer.removeEventListener('paste', onPaste);
+        }
+    }
+
+}
